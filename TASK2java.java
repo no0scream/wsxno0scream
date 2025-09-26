e6689af329891cc21b4908853fd8b521b4e2c25b
no0scream <130081302+no0scream@users.noreply.github.com>
2025-09-26 18:52:13 +0300

    Update TASK2java.java

--- TASK2java.java
+++ TASK2java.java
@@ -1,4 +1,4 @@
 import java.util.HashMap;
 HashMap<String, Integer> map = new HashMap<>();
-map.put("apple", 5);
-System.out.println(map.get("apple"));  // 5
+map.put("key", 10);
+int value = map.get("key");
